8669eae76fa84fe9f9d9eb8b9c5787c656ad518a
MarcoGorelli <>
2023-02-01 13:19:38 +0000

    wip

--- polars/polars-arrow/src/kernels/time.rs
+++ polars/polars-arrow/src/kernels/time.rs
@@ -47,6 +47,17 @@ fn from_tz_to_tz(from_tz: Tz, to_tz: Tz, ndt: NaiveDateTime) -> NaiveDateTime {
         .with_timezone(&to_tz)
         .naive_local()
 }
+#[cfg(feature = "timezones")]
+fn from_to<T1: TimeZone, T2: TimeZone>(from_tz: T1, to_tz: T2, ndt: NaiveDateTime) -> impl Fn(NaiveDateTime) -> NaiveDateTime {
+    fn inner<T1: TimeZone, T2: TimeZone>(from_tz: T1, to_tz: T2, ndt: NaiveDateTime) -> NaiveDateTime{
+        from_tz
+        .from_local_datetime(&ndt)
+        .unwrap()
+        .with_timezone(&to_tz)
+        .naive_local()
+    }
+    |ndt| inner(from_tz, to_tz, ndt)
+}
 fn convert_millis(value: i64, op: impl Fn(NaiveDateTime) -> NaiveDateTime) -> i64 {
     let ndt = timestamp_ms_to_datetime(value);
     op(ndt).timestamp_millis()
@@ -60,6 +71,7 @@ fn convert_nanos(value: i64, op: impl Fn(NaiveDateTime) -> NaiveDateTime) -> i64
     op(ndt).timestamp_nanos()
 }
 
+
 #[cfg(feature = "timezones")]
 pub fn cast_timezone(
     arr: &PrimitiveArray<i64>,
@@ -67,36 +79,26 @@ pub fn cast_timezone(
     from: String,
     to: String,
 ) -> ArrayRef {
+    let conversion_func2 = match tu {
+        TimeUnit::Millisecond => convert_millis,
+        TimeUnit::Microsecond => convert_micros,
+        TimeUnit::Nanosecond => convert_nanos,
+        _ => unreachable!(),
+    };
     match from.parse::<chrono_tz::Tz>() {
         Ok(from_tz) => match to.parse::<chrono_tz::Tz>() {
             Ok(to_tz) => {
-                let conversion_func = match tu {
-                    TimeUnit::Millisecond => convert_millis,
-                    TimeUnit::Microsecond => convert_micros,
-                    TimeUnit::Nanosecond => convert_nanos,
-                    _ => unreachable!(),
-                };
                 Box::new(unary(
                     arr,
-                    |value| conversion_func(value, |value| from_tz_to_tz(from_tz, to_tz, value)),
+                    |value| conversion_func2(value, from_to(from_tz, to_tz)),
                     ArrowDataType::Int64,
                 ))
             }
             Err(_) => match parse_offset(&to) {
                 Ok(to_tz) => {
-                    let conversion_func = match tu {
-                        TimeUnit::Millisecond => convert_millis,
-                        TimeUnit::Microsecond => convert_micros,
-                        TimeUnit::Nanosecond => convert_nanos,
-                        _ => unreachable!(),
-                    };
                     Box::new(unary(
                         arr,
-                        |value| {
-                            conversion_func(value, |value| {
-                                from_tz_to_fixed_offset(from_tz, to_tz, value)
-                            })
-                        },
+                        |value| conversion_func2(value, from_to(from_tz, to_tz)),
                         ArrowDataType::Int64,
                     ))
                 }
