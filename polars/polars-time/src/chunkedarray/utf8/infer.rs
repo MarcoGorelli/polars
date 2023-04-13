f075c9d0f9986d79e48806e29ccaaad9aec257f9
MarcoGorelli <>
2023-04-13 14:57:09 +0100

    how about not converting to str?

--- polars/polars-time/src/chunkedarray/utf8/infer.rs
+++ polars/polars-time/src/chunkedarray/utf8/infer.rs
@@ -195,28 +195,19 @@ impl Pattern {
         match self {
             Pattern::DateDMY => match DATE_DMY_BYTES_RE.captures(val) {
                 Some(search) => (1..=12).contains(
-                    &std::str::from_utf8(&search.name("month").unwrap().as_bytes())
-                        .unwrap()
-                        .parse::<u8>()
-                        .unwrap(),
+                    &search.name("month").unwrap().as_bytes().iter().fold (0, |acc, c| acc*10 + (c - '0' as u8))
                 ),
                 None => false,
             },
             Pattern::DateYMD => match DATE_YMD_BYTES_RE.captures(val) {
                 Some(search) => (1..=12).contains(
-                    &std::str::from_utf8(&search.name("month").unwrap().as_bytes())
-                        .unwrap()
-                        .parse::<u8>()
-                        .unwrap(),
+                    &search.name("month").unwrap().as_bytes().iter().fold (0, |acc, c| acc*10 + (c - '0' as u8))
                 ),
                 None => false,
             },
             Pattern::DatetimeDMY => match DATETIME_DMY_BYTES_RE.captures(val) {
                 Some(search) => (1..=12).contains(
-                    &std::str::from_utf8(&search.name("month").unwrap().as_bytes())
-                        .unwrap()
-                        .parse::<u8>()
-                        .unwrap(),
+                    &search.name("month").unwrap().as_bytes().iter().fold (0, |acc, c| acc*10 + (c - '0' as u8))
                 ),
                 None => false,
             },
