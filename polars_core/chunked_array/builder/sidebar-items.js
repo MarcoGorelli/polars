924ed80e8126ed1fc6e6dee0c23d079cb369fd34
MarcoGorelli <marcogorelli@protonmail.com>
2024-09-30 19:27:02 +0000

    Deploying to gh-pages from @ MarcoGorelli/polars@4c6d5013ce11ba7852a76d90f964e060f62cd2a9 🚀

---
+++ polars_core/chunked_array/builder/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["fixed_size_list","list"],"struct":["BinaryChunkedBuilder","BinaryChunkedBuilderCow","BooleanChunkedBuilder","PrimitiveChunkedBuilder","Utf8ChunkedBuilder","Utf8ChunkedBuilderCow"],"trait":["ChunkedBuilder","NewChunkedArray"]};
